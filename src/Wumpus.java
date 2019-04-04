65f5387272033506387c16b6099c46cf579d5fc2
juliannechen <julianneychen@gmail.com>
2019-04-04 11:14:08 -0700

    created wumpus class

--- src/Wumpus.java
+++ src/Wumpus.java
@@ -1,10 +1,18 @@
-public class Wumpus extends Creature{
-    public Wumpus(Level.Room currentRoom){
-        this.currentRoom = currentRoom;
+public class Wumpus extends Creature {
+    Player player;
+
+    public Wumpus(Level.Room currentRoom, String name, String description, Player p) {
+        super(currentRoom, name, description);
+        player = p;
     }
 
     @Override
     public void move() {
+        if (isPlayerAdjacent(player)) {
+
+        } else {
+            System.out.println(this.getName() + " did not move");
+        }
 
     }
 }
